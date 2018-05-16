b5ebbc4e36c75b1f95a44285457764a93e06e0e9
pocari <caffelattenonsugar@gmail.com>
2018-05-16 19:42:13 +0900

    いろいろ修正

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'parslet'
+require 'json'
 require 'pry-byebug'
 
 class BaseParser < Parslet::Parser
@@ -19,92 +20,138 @@ class QueryParser < BaseParser
   root(:and_queries)
 
   rule(:and_queries) { (query.as(:and_query) >> (space >> query.as(:and_query)).repeat).as(:and_queries) >> space?}
-  rule(:query) { field.as(:field_list) >> or_conditions }
+  rule(:query) { field.as(:field_list) >> and_conditions }
 
   rule(:field) { multi_field >> str(':') >> space? }
   rule(:multi_field)  { identifier.as(:field) >> (str(',') >> identifier.as(:field)).repeat }
 
-  rule(:or_conditions)  { (and_conditions >> (or_op  >> and_conditions).repeat(1) | and_conditions).as(:or_conditions)}
-  rule(:and_conditions) { (condition      >> (and_op >> condition     ).repeat(1) | condition     ).as(:and_conditions) }
+  # 普通の言語とandとorの優先度が逆になっている(andの方が優先度が低い)ので注意
+  rule(:and_conditions) { (or_conditions >> (and_op >> or_conditions).repeat).as(:and_conditions)}
+  rule(:or_conditions)  { (condition     >> (or_op  >> condition    ).repeat).as(:or_conditions) }
   rule(:or_op) { (space >> str('or') >> space) | space }
-  rule(:and_op) { space >> str('and') >> space}
+  rule(:and_op) { space >> str('and') >> space }
 
   rule(:condition) { (quoted_string | raw_condition).as(:condition) >> str(':').absent? }
-  rule(:raw_condition) {match('[^:\s]').repeat(1)}
+  # ここで特別にandを無視しておかないと検索ワードとしてandが含まれてしまうため除く
+  # andを値として使いたい場合は"", ''で囲ってquoted_string側に入るようにする
+  rule(:raw_condition) { str('and').absent? >> match('[^:\s]').repeat(1) }
   rule(:identifier) { match('[_0-9a-zA-Z]').repeat(1) }
 end
 
-class SimplyfySyntaxTreeTransformer < Parslet::Transform
+EsQuery = Struct.new(:bool) do
+  def eval
+    # p [:EsQuery, bool]
+    {
+      query: bool.eval
+    }
+  end
+end
+
+# "bool": {
+#   "must" {
+#     ...
+#   }
+# }
+MustQueries = Struct.new(:queries) do
+  def eval
+    # p [:Must, queries.size, queries]
+    {
+      bool: {
+        must: queries.map(&:eval)
+      }
+    }
+  end
+end
+
+# "bool": {
+#   "should" {
+#     ...
+#   }
+# }
+ShouldQueries = Struct.new(:queries) do
+  def eval
+    # p [:Should, queries.size, queries]
+    {
+      bool: {
+        should: queries.map(&:eval)
+      }
+    }
+  end
+end
+
+# "terms" : {
+#   #{field}: #{values}
+#  }
+Terms = Struct.new(:values, :field) do
+  def eval
+    # p [:Terma, field, values]
+    {
+      terms: {
+        field => values
+      }
+    }
+  end
+end
+
+class ElasticSearchQueryTransformer < Parslet::Transform
   def self.simple_to_s_rule(sym)
     rule(sym => simple(:x)) { x.to_s }
   end
   simple_to_s_rule(:condition)
   simple_to_s_rule(:field)
   simple_to_s_rule(:str)
-  simple_to_s_rule(:and_conditions)
-  simple_to_s_rule(:and_query)
+  rule(and_query: subtree(:x)) { x }
+  rule(or_conditions: subtree(:tree)) {
+    # p [:or_conditions, tree]
+    tree2 = tree.is_a?(Array) ? tree : [tree]
+    Terms.new(tree2)
+  }
 
   rule(
-    field_list: simple(:x),
-    or_conditions: subtree(:y)
+    field_list: subtree(:x),
+    and_conditions: subtree(:y)
   ) {
-    {
-      field_list: Array(x),
-      or_conditions: [y]
+    # p [:and_cond, x, y, y.class]
+
+    # Array(y) だと結果が変わるので注意
+    xx = x.is_a?(Array) ? x : [x]
+    yy = y.is_a?(Array) ? y : [y]
+
+    and_conditions = xx.map {|field|
+      queries = yy.map {|term|
+        term.dup.tap {|obj|
+          obj.field = field
+        }
+      }
+      MustQueries.new(queries)
     }
+    ShouldQueries.new(and_conditions)
+  }
+
+  rule(and_queries: subtree(:queries)) {
+    #p [:and_queries, queries]
+    normalized = queries.is_a?(Array) ? queries : [queries]
+    eq = EsQuery.new(MustQueries.new(normalized))
+    # p [:es_query, eq]
+    eq
   }
 end
 
-class ElasticSearchQueryTransformer < Parslet::Transform
-end
-
-
-# % echo "title,content: hoge piyo subtitleu:aaa and \"bbb bb2\" and 'ccc ddd'" | be ruby main.rb
-# ------------------------ raw query
-# title,content: hoge piyo subtitleu:aaa and "bbb bb2" and 'ccc ddd'
-# ------------------------ raw => syntax tree
-# {:and_queries=>
-#   [{:and_query=>
-#      {:field_list=>[{:field=>"title"@0}, {:field=>"content"@6}],
-#       :or_conditions=>
-#        [{:and_conditions=>{:condition=>"hoge"@15}},
-#         {:and_conditions=>{:condition=>"piyo"@20}}]}},
-#    {:and_query=>
-#      {:field_list=>{:field=>"subtitleu"@25},
-#       :or_conditions=>
-#        {:and_conditions=>
-#          [{:condition=>"aaa"@35},
-#           {:condition=>{:str=>"bbb bb2"@44}},
-#           {:condition=>{:str=>"ccc ddd"@58}}]}}}]}
-# ------------------------ syntax tree => simplyfy AST
-# {:and_queries=>
-#   [{:and_query=>
-#      {:field_list=>["title", "content"], :or_conditions=>["hoge", "piyo"]}},
-#    {:and_query=>
-#      {:field_list=>["subtitleu"],
-#       :or_conditions=>[{:and_conditions=>["aaa", "bbb bb2", "ccc ddd"]}]}}]}
-# ------------------------ simplyfy AST => ElasticSearch Query
-# not implemented
-
 raw = STDIN.read.chomp
-puts "------------------------ raw query"
-puts raw
-
-puts "------------------------ raw => syntax tree"
+# puts "------------------------ raw query"
+# puts raw
+# 
 begin
   parsed = QueryParser.new.parse(raw)
-  pp parsed
+#  puts "------------------------ raw => syntax tree"
+#  pp parsed
 rescue Parslet::ParseFailed => failure
   puts failure.parse_failure_cause.ascii_tree
   raise failure
 end
+# 
+# puts "------------------------ syntax tree => es query"
+ast = ElasticSearchQueryTransformer.new.apply(parsed)
+#pp [:ast, ast]
 
-puts "------------------------ syntax tree => simplyfy AST"
-ast = SimplyfySyntaxTreeTransformer.new.apply(parsed)
-pp ast
-
-puts "------------------------ simplyfy AST => ElasticSearch Query"
-# query = ElasticSearchQueryTransformer.new.apply(ast)
-# pp query
-puts "not implemented"
-
+puts ast.eval.to_json
